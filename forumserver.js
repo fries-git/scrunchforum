308c8d0a3b46305306295593028b28e7135e1e57
fries <fries_09@hotmail.com>
2025-09-30 06:33:55 -0400

    Update forumserver.js

--- forumserver.js
+++ forumserver.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 let posts = [];
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // Load posts from file
